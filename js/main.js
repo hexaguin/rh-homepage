a411947e96d37f3f99d0a9a49156e9985749f960
Raelynn Hattie <hexaguin@gmail.com>
2021-12-31 04:58:57 -0700

    Indented link selector for Swup

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const swup = new Swup({
     animationSelector: '[class*="page-transition-"]',
     animateHistoryBrowsing: true,
     linkSelector:
-    'a[href^="' +
-    window.location.origin +
-    '"]:not([target^="_blank"]), a[href^="/"]:not([target^="_blank"]), a[href^="#"]:not([target^="_blank"])'
+        'a[href^="' +
+        window.location.origin +
+        '"]:not([target^="_blank"]), a[href^="/"]:not([target^="_blank"]), a[href^="#"]:not([target^="_blank"])'
 });
